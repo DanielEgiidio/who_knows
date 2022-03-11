c70b457ce9ea926dec2c4024386228184da28252
DanielEgidiio <danielegidioti@gmail.com>
2022-03-11 20:39:38 -0300

    setup footer e routes

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+
+import { Results } from "./Results";
 
 export const Routes = () => {
-  return <div>Routes</div>;
+  return (
+    <div className="p-4">
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/pesquisar" />
+        </Route>
+        <Route exact path={["/pesquisar", "/imagens", "/novidades", "/videos"]}>
+          <Results />
+        </Route>
+      </Switch>
+    </div>
+  );
 };
